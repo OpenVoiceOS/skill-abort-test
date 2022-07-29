9cbcb33c2bbfe83032807f9ba9765a8537b64b4a
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-07-29 12:26:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 from setuptools import setup
+from os import getenv, path, walk
+
+
+def find_resource_files():
+    resource_base_dirs = ("locale", "ui", "vocab", "dialog", "regex")
+    base_dir = path.dirname(__file__)
+    package_data = ["skill.json"]
+    for res in resource_base_dirs:
+        if path.isdir(path.join(base_dir, res)):
+            for (directory, _, files) in walk(path.join(base_dir, res)):
+                if files:
+                    package_data.append(
+                        path.join(directory.replace(base_dir, "").lstrip('/'),
+                                  '*'))
+    return package_data
+
 
 # skill_id=package_name:SkillClass
 PLUGIN_ENTRY_POINT = 'ovos-tskill-abort.openvoiceos=ovos_tskill_abort:TestAbortSkill'
@@ -14,7 +30,7 @@ setup(
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
     package_dir={"ovos_tskill_abort": ""},
-    package_data={'ovos_tskill_abort': ['locale/*']},
+    package_data={'ovos_tskill_abort': find_resource_files()},
     packages=['ovos_tskill_abort'],
     include_package_data=True,
     install_requires=["ovos-workshop"],
